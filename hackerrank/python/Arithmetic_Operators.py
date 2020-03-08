4ec7006590bb9aa68f2abf348068c1b3c7b9aa0c
Vex <sanyamsxn1998@gmail.com>
2020-03-08 20:40:22 +0530

    Arithmetic_Operators.py

---
+++ hackerrank/python/Arithmetic_Operators.py
@@ -0,0 +1,10 @@
+n_1=int(input())
+n_2=int(input())
+addition=[n_1,n_2]
+print(sum(addition))                   #sum(iterable,start): sum all no.s in list + start
+                                       #iterable can be list, tuple ,dict but should contain no.
+sub=(n_1-n_2)
+print(sub)
+
+prod=(n_1*n_2)
+print(prod)
